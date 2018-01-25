bf28b7976ed1de2b32938fb73ccd88ddf9b8317d
Oleksandr Pets <opets@desire2learn.com>
2018-01-25 15:51:53 +0200

    init

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary1;
+using ClassLibrary2;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class2.Message);
+			//Class2.ZipTest();
+			Class2.ZipCompressTest();
+
+		}
+    }
+}
